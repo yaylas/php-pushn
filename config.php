30a767128913c8c5f6f94480c386824831709e90
Selçuk Yayla <yayla_selcuk@hotmail.com>
2014-03-19 10:18:46 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 /*
  * Google API Key
  */
-define("GOOGLE_API_KEY", ""); // Place your Google API Key
-define("REG_ID", "");
+define("GOOGLE_API_KEY", "198432919479"); // Place your Google API Key
+define("REG_ID", "APA91bGuXe7BcJYMyNwfMjpF4oPgBwlNODA1rF3Pi-RK37wfF9-T1NbGFDq8T1c16nrCPK_75YplE6MPhG4iXiUKqyIHziO_ZLY_0UoZvl9z3R22K7GeF-raDD6TsV_92_Atp55IDDfNFTWxKOms56_rkRXXL5ydCzCExit_PcfEEFEeQPewjrM");
 ?>
